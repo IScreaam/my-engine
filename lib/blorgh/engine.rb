acce8fb0233b930416722a503295552afd4e9a5d
Vlad <vladislav.belyaev.93@gmail.com>
2015-07-27 11:13:09 +0300

    first commit

---
+++ lib/blorgh/engine.rb
@@ -0,0 +1,12 @@
+module Blorgh
+  class Engine < ::Rails::Engine
+    isolate_namespace Blorgh
+
+		config.generators do |g|
+		  g.test_framework      :rspec,        :fixture => false
+		  g.fixture_replacement :factory_girl, :dir => 'spec/factories'
+		  g.helper false
+		  g.template_engine :slim
+		end
+  end
+end
